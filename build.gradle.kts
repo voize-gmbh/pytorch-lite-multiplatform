b175c05405985cc73ed334d5155c844ec7fd9a13
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2022-06-29 20:22:49 +0200

    v0.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "de.voize"
-version = "0.3.3"
+version = "0.4.0"
 
 repositories {
     google()
@@ -47,7 +47,7 @@ kotlin {
         summary = "Kotlin Multiplatform wrapper for PyTorch Lite"
 
         pod("PLMLibTorchWrapper") {
-            version = "0.3.3"
+            version = "0.4.0"
             source = path(project.file("ios/LibTorchWrapper"))
         }
 
