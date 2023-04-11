192afec4838e537ab37c18efeb4e712d6a47f24b
Erik Ziegler <erik@voize.de>
2023-04-11 16:45:17 +0200

    Add simulator tests for Android

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,11 @@ kotlin {
         }
         val androidUnitTest by getting {
             dependencies {
-                implementation("junit:junit:4.13")
+                implementation("junit:junit:4.13.2")
+                implementation("androidx.test:core:1.5.0")
+                implementation("androidx.test:rules:1.5.0")
+                implementation("androidx.test:runner:1.5.2")
+                implementation("androidx.test.ext:junit:1.1.5")
             }
         }
         all {
@@ -165,6 +169,7 @@ android {
     defaultConfig {
         minSdkVersion(24)
         targetSdkVersion(31)
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
